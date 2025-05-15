436e96ba92df7db332c42913929ccf17d5626c3a
MoQuant <112667516+MoQuant@users.noreply.github.com>
2025-05-14 21:37:46 -0400

    Update anova.cpp

--- anova.cpp
+++ anova.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <vector>
 
+// Prints 2D vector
 void PRINTM(std::vector<std::vector<double>> x){
     for(auto & t : x){
         for(auto & u : t){
@@ -11,6 +12,7 @@ void PRINTM(std::vector<std::vector<double>> x){
     }
 }
 
+// Matrix Multiplication
 std::vector<std::vector<double>> MMULT(std::vector<std::vector<double>> x,
                                        std::vector<std::vector<double>> y)
 {
@@ -32,6 +34,7 @@ std::vector<std::vector<double>> MMULT(std::vector<std::vector<double>> x,
     return result;
 }
 
+// Transposes a 2D vector
 std::vector<std::vector<double>> TRANSPOSE(std::vector<std::vector<double>> z)
 {
     std::vector<std::vector<double>> X;
@@ -46,6 +49,7 @@ std::vector<std::vector<double>> TRANSPOSE(std::vector<std::vector<double>> z)
     return X;
 }
 
+// Computes the inverse of a matrix using Gaussian Elimination
 std::vector<std::vector<double>> INVERSE(std::vector<std::vector<double>> x)
 {
     std::vector<std::vector<double>> I;
@@ -97,10 +101,13 @@ std::vector<std::vector<double>> INVERSE(std::vector<std::vector<double>> x)
     return I;
 }
 
+// Generates the ANOVA table
 void ANOVA(std::vector<std::vector<double>> BX, std::vector<std::vector<double>> Y)
 {
     std::vector<std::vector<double>> X;
     std::vector<double> temp;
+
+    // Add a constant 1 for the intercept beta 0
     for(int i = 0; i < BX.size(); ++i){
         temp.clear();
         temp.push_back(1.0);
@@ -110,43 +117,56 @@ void ANOVA(std::vector<std::vector<double>> BX, std::vector<std::vector<double>>
         X.push_back(temp);
     }
 
-    
+    // Declare 2D vectors for matrix operations
     std::vector<std::vector<double>> beta, XTX, IXTX, XTY, YHat;
+
+    // Declare numerical variables for statistical results
     double ymu = 0, rss, tss, ess, F, rsq, adjrsq;
 
+    // Gather the dimensions of the dataframe vector
     int n = X.size(), m = X[0].size() - 1;
 
+    // Use matrix algebra to compute the beta vector
     XTX = MMULT(TRANSPOSE(X), X);
     IXTX = INVERSE(XTX);
     XTY = MMULT(TRANSPOSE(X), Y);
     beta = MMULT(IXTX, XTY);
 
     YHat = MMULT(X, beta);
-    
+
+    // Solve for residual sum of squares
     for(int i = 0; i < Y.size(); ++i){
         ymu += Y[i][0];
         rss += pow(Y[i][0] - YHat[i][0], 2);
     }
     ymu /= (double) Y.size();
+
+    // Solve for TSS
     for(int i = 0; i < Y.size(); ++i){
         tss += pow(Y[i][0] - ymu, 2);
     }
     ess = tss - rss;
 
+    // Compute the F-Statistic for the table
     F = (ess/(m))/(rss/(n - m - 1));
+
+    // Compute the models R-Squared and Adjusted R-Squared
     rsq = 1 - rss/tss;
     adjrsq = 1 - (1 - rsq)*(n - 1)/(n - m - 1);
 
+    // Compute the standard error for all betas
     double factor = rss / (n - m - 1);
     std::vector<double> se, tstat;
     for(int i = 0; i < IXTX.size(); ++i){
         se.push_back(pow(factor*IXTX[i][i], 0.5));
     }
 
+    // Compute test statistic for each beta coeffecient
     for(int i = 0; i < se.size(); ++i){
         tstat.push_back(beta[i][0]/se[i]);
     }
-    
+
+    // Print out the ANOVA table
     std::cout << "ANOVA TABLE" << std::endl;
     std::cout << "RSquared: " << rsq << std::endl;
     std::cout << "Adj. RSquared: " << adjrsq << std::endl;
@@ -163,6 +183,7 @@ int main()
 {
     std::vector<std::vector<double>> X, y, M;
 
+    // Sample dataframe matrix
     X = {{1, 2, 3},
          {4, 2, 1},
          {5, 7, 2},
@@ -174,6 +195,7 @@ int main()
          {8, 2, 1},
          {4, 6, 9}};
 
+    // Sample output matrix
     y = {{2},{3},{5},{8},{13},{21},{34},{55},{89},{144}};
 
     ANOVA(X, y);
